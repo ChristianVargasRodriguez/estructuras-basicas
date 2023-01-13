2f968167f765876b9eee54a263305f70b088e84c
Christian Vargas <christianj.vrgs@gmail.com>
2023-01-13 13:11:19 -0300

    Mi primer commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var clicks1 = 0;
+    function like1() {
+        clicks1 += 1;
+        document.getElementById("clicks1").innerHTML = clicks1;
+    }
+
+var clicks2 = 0;
+    function like2() {
+        clicks2 += 1;
+        document.getElementById("clicks2").innerHTML = clicks2;
+    }
+
+var clicks3 = 0;
+    function like3() {
+        clicks3 += 1;
+        document.getElementById("clicks3").innerHTML = clicks3;
+    }
+
